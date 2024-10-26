ac302aa604f6048cc557b6d5e5466c65d8d6b696
sruraj <srusinha092@gmail.com>
2024-10-26 14:50:19 +0530

    initial commot

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setcolor] = useState("olive");
+
+  return (
+    <div className="w-full h-screen"
+      style={{backgroundColor: color}}
+    >
+      <div className="fixed flex flex-wrap
+      justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3
+        shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+          onClick={() => setcolor("red")} // on clicking the colour is changed: returnning the function
+          className="outline-none px-4 py-1
+          rounded-full text-white shadow-lg"
+          style={{backgroundColor: "red"}}>red</button>
+          <button
+          onClick={() => setcolor("green")} // return function to onClick()
+          className="outline-none px-4 py-1
+          rounded-full text-white shadow-lg"
+          style={{backgroundColor: "green"}}>green</button>
+          <button
+          onClick={() => setcolor("blue")} // return function to onClick()
+          className="outline-none px-4 py-1
+          rounded-full text-white shadow-lg"
+          style={{backgroundColor: "blue"}}>blue</button>
+        </div>
+      </div>  
+
+    </div>
+  );
+}
+
+export default App;
